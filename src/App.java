1dec31b38ee2bc946653e9486058053e4ecfe131
Emerson <emersonnjunior2006@gmail.com>
2025-04-10 19:30:22 -0300

    add

--- src/App.java
+++ src/App.java
@@ -7,5 +7,12 @@ public class App {
 
         System.out.println("aula 11-Enum");
         System.out.println(Cidade.CURITIBA);
+
+        Cidade cidade = Cidade.PONTA_GROSSA;
+        System.out.println(cidade);
+
+        cidade.setDdd(55);
+        Cidade novaCidade = Cidade.PONTA_GROSSA;
+        System.out.println("\n" + cidade + "\n\n" + novaCidade);
     }
 }
--- src/model/Cidade.java
+++ src/model/Cidade.java
@@ -1,7 +1,35 @@
 package model;
 
 public enum Cidade {
-    CURITIBA,
-    PONTA_GROSSA,
-    SAO_JOSE_DOS_PINHAIS;
+    CURITIBA(41, "curitiba"),
+    PONTA_GROSSA(42, "ponta grossa"),
+    SAO_JOSE_DOS_PINHAIS(42, "sao jose dos pinhais");
+
+    private int ddd;
+    private String nome;
+
+    private Cidade(int ddd, String nome) {
+        this.ddd = ddd;
+        this.nome = nome;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    @Override
+    public String toString(){
+        return "("+ ddd + ") - " +nome;
+    }
 }
