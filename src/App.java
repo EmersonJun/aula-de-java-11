dc82161b3b3a0e02fe8fc8307ed5adbb6389d0a8
Emerson <emersonnjunior2006@gmail.com>
2025-04-10 19:20:26 -0300

    enum

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import model.Cidade;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+        System.out.println("aula 11-Enum");
+        System.out.println(Cidade.CURITIBA);
+    }
+}
---
+++ src/model/Cidade.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum Cidade {
+    CURITIBA,
+    PONTA_GROSSA,
+    SAO_JOSE_DOS_PINHAIS;
+}
